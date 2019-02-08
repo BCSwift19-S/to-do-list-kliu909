622ad0c13c51141931beea4c72e8a045d9d48db2
Katrina Liu <katrina@katrinas-mbp.bc.edu>
2019-02-08 01:05:32 -0500

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Katrina Liu on 2/8/19.
+//  Copyright © 2019 Katrina Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
